050f0783d5e3bc77f9a899658b428efeeed0e11a
Pierpaolo Frasa <pfrasa@gmail.com>
2018-10-16 15:26:10 +0200

    allow marking book available

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  AlreadyAvailableError = Class.new StandardError
   InvalidStatusError = Class.new StandardError
 
   def index
@@ -16,6 +17,26 @@ class BooksController < ApplicationController
     render json: {
       code: "invalid_status_filter",
       error: "The specified status filter is invalid. Please use one of #{Book::VALID_STATUS.join(", ")}."
+    }, status: 422
+  end
+
+  def create
+    book_id = params[:book_request_id]
+
+    book = Book.find(book_id)
+    raise AlreadyAvailableError if book.status != "requested"
+    book.update!(status: "available")
+
+    render json: book
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      code: "book_not_found",
+      error: "No book with that ID has been requested."
+    }, status: 404
+  rescue AlreadyAvailableError
+    render json: {
+      code: "already_available",
+      error: "This book is already available (or borrowed)."
     }, status: 422
   end
 
